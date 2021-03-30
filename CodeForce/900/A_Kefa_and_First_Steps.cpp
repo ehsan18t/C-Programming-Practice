9e56e30e7db4a29af6fd0eb79d4ddce58cd681b8
Ahsan40 <30434801+Ahsan40@users.noreply.github.com>
2021-03-31 02:04:53 +0600

    Update A_Kefa_and_First_Steps.cpp

--- CodeForce/900/A_Kefa_and_First_Steps.cpp
+++ CodeForce/900/A_Kefa_and_First_Steps.cpp
@@ -2,8 +2,26 @@
 
 int main()
 {
-    int n, result;
+    int n;
+    int count = 1;
+    int highest = 0;
     std::cin >> n;
-    std::cout << result << std::endl;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        std::cin >> arr[i];
+    }
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] <= arr[i + 1] && i + 1 != n)
+            count++;
+        else
+        {
+            if (count > highest)
+                highest = count;
+            count = 1;
+        }
+    }
+    std::cout << highest << std::endl;
     return 0;
 }

55ddf2552cb7f9a666f1ba1d86984fa17a88be2f
Ahsan40 <30434801+Ahsan40@users.noreply.github.com>
2021-05-08 21:36:38 +0600

    Update C_Ringo_s_Favorite_Numbers_2.cpp

--- AtCodder Contests/4th Contest/C_Ringo_s_Favorite_Numbers_2.cpp
+++ AtCodder Contests/4th Contest/C_Ringo_s_Favorite_Numbers_2.cpp
@@ -5,29 +5,34 @@
 #include "./Debugging Tools/MemView.cpp"
 #endif
 
-// Don't works
+// Don't works [TLE]
 
 int main()
 {
     int n;
     int counter = 0;
-    std::cin >> n;
+    std::cin >> n; // input to n
     int num[n];
 
     for (int i = 0; i < n; i++)
-        std::cin >> num[i];
+        std::cin >> num[i]; // input to num[i]
 
     for (int i = 0; i < n; i++)
     {
-        for (int j = i; j < n; j++)
+        for (int j = i + 1; j < n; j++)
         {
             int tmp = num[i] - num[j];
             if (tmp % 200 == 0)
             {
                 counter++;
+                // debug
+                // dbg(num[i]);
+                // dbg(num[j]);
+                // dbg(counter);
+                // std::cout << std::endl;
             }
         }
     }
-    std::cout << counter << std::endl;
+    std::cout << counter << std::endl; // ouput counter
     return 0;
 }

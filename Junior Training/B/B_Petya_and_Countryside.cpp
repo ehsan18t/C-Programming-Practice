b6cfc1b835529a232aa2478235c8583b4e556c53
Ahsan40 <30434801+Ahsan40@users.noreply.github.com>
2021-08-19 19:28:32 +0600

    Create B_Petya_and_Countryside.cpp

---
+++ Junior Training/B/B_Petya_and_Countryside.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+#ifndef ONLINE_JUDGE
+#include "../Debugging Tools/dbg.cpp"
+#include "../Debugging Tools/MemView.cpp"
+#endif
+
+int main()
+{
+    int n, max = 0, last = 0;
+    std::cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        std::cin >> arr[i];
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+
+        last = 0;
+        int current = arr[i];
+        int j = i;
+        while (current <= arr[j] && j < n)
+        {
+            current = arr[j];
+            last++;
+            j++;
+        }
+        while (current >= arr[j] && j < n)
+        {
+            current = arr[j];
+            last++;
+            j++;
+        }
+
+        if (last > max)
+            max = last;
+    }
+    std::cout << max << std::endl;
+    return 0;
+}

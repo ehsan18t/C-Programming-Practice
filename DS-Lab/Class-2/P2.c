4441669684e44567d52323d20a41f478152e485e
Ahsan Khan <30434801+Ahsan40@users.noreply.github.com>
2021-11-13 11:55:20 +0600

    Update P2.c

--- DS-Lab/Class-2/P2.c
+++ DS-Lab/Class-2/P2.c
@@ -1,14 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-// Replacement sort
+// random input generate then sort
+// using Replacement sort.
 
 int main()
 {
-    int n = 4;
+    int n = 100;
     int arr[n];
 
+    generateRandom(arr, n);
+
+    // before sort
+    printf("\n\nBefore sort:\n");
     for(int i = 0; i < n; i++)
-        scanf("%d", &arr[i]);
+        printf("%d ", arr[i]);
+//    for(int i = 0; i < n; i++)
+//        scanf("%d", &arr[i]);
 
     for (int i = 0; i < n - 1; i++)
     {
@@ -22,7 +30,18 @@ int main()
             }
         }
     }
+    //after sort
+    printf("\n\nAfter sort:\n\n");
     for(int i = 0; i < n; i++)
         printf("%d ", arr[i]);
     return 0;
+}
+
+void generateRandom(int arr[], int len)
+{
+    srand(time(NULL));
+    for (int i = 0; i < len; i++)
+    {
+        arr[i] = rand() % 1000;
+    }
 }

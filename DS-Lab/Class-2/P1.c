f77fe533377caf78760e03a1e2f121dd73be64ec
Ahsan Khan <30434801+Ahsan40@users.noreply.github.com>
2021-11-13 11:17:52 +0600

    Update P1.c

--- DS-Lab/Class-2/P1.c
+++ DS-Lab/Class-2/P1.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
 
+// take some input in an array and
+// find the minimum among the input.
+
+int main()
+{
+    int n = 5;
+    int minIndex = 0;
+    int arr[n];
+
+    // taking input
+    for(int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    // finding min index
+    for (int i = 1; i < n; i++)
+        if(arr[1] < arr[minIndex])
+            minIndex = i;
+
+    // printing output
+    printf("%d", arr[minIndex]);
+    return 0;
+}

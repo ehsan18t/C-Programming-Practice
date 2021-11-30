add322bde743aa5e5c73e912334f23bb8c21b3f5
Ahsan Khan <nobody.ahsan@gmail.com>
2021-11-30 14:40:21 +0600

    ds-lab-5 p1

---
+++ DS-Lab/Class-5/P1.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Add data to at the end of linked list
+
+struct node
+{
+    int data;
+    struct node *next;
+} typedef node;
+
+void display(node *head)
+{
+    node *temp = head;
+    while (temp != NULL)
+    {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+    printf("\n");
+}
+
+int main()
+{
+    node *start = NULL, *prev = NULL, *temp;
+    int ans;
+    printf("Do you want to add data: ");
+    scanf("%d", &ans);
+    while (ans == 1)
+    {
+        if (start == NULL)
+        {
+            start = (node *)malloc(sizeof(node));
+            printf("Enter Data: ");
+            scanf("%d", &start->data);
+            start->next = NULL;
+            prev = start;
+        }
+        else
+        {
+            temp = (node *)malloc(sizeof(node));
+            printf("Enter data: ");
+            scanf("%d", &temp->data);
+            prev->next = temp;
+            temp->next = NULL;
+            prev = temp;
+        }
+        printf("Do you want to add data: ");
+        scanf("%d", &ans);
+    }
+    display(start);
+    return 0;
+}

fda01ae9386a33cf79a7f671b86ea1d985629341
Ahsan Khan <nobody.ahsan@gmail.com>
2022-04-13 16:47:53 +0600

    Update CPH_Template.cpp

--- Junior Training/CPH_Template.cpp
+++ Junior Training/CPH_Template.cpp
@@ -13,6 +13,7 @@
 #define PI 2 * acos(0.0)
 #define INF LLONG_MAX
 #define sqr(n) (n * n)
+#define nCr(n, k) std::exp(std::lgamma(n + 1) - std::lgamma(k + 1) - std::lgamma(n - k + 1))
 
 // In/out
 #ifndef ONLINE_JUDGE
@@ -32,130 +33,14 @@
 #define pb push_back
 #define lg length()
 #define sz size()
-#define yes cout << "Yes" << endl;
-#define no cout << "No" << endl;
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
 
 // Opt
 #define sync                      \
     ios_base::sync_with_stdio(0); \
     cin.tie(0);                   \
     cout.tie(0);
-
-/* #region(collapsed) DEBUG */
-// Debug Data
-// Simple Debugger by UIU CP Club
-// I've modified a littile bit for better view
-
-// for 2d array
-#define dbgA2(A, n, m)                           \
-    {                                            \
-        std::cout << "--> " << #A << " =\n    "; \
-        for (int i = 0; i < n; i++)              \
-        {                                        \
-            std::cout << "[" << i << "] -> { ";  \
-            for (int j = 0; j < m; j++)          \
-            {                                    \
-                std::cout << A[i][j] << " ";     \
-            }                                    \
-            std::cout << "}\n    ";              \
-        }                                        \
-        std::cout << "\n";                       \
-    }
-
-// for array
-#define dbgA(A, n)                            \
-    {                                         \
-        std::cout << "--> " << #A << " = { "; \
-        for (int i = 0; i < n; i++)           \
-            std::cout << A[i] << " ";         \
-        std::cout << "}\n";                   \
-    }
-
-// for variables
-#define dbg(args...)                                  \
-    {                                                 \
-        std::string sss(#args);                       \
-        sss += ',';                                   \
-        std::cout << "--> ";                          \
-        debugger::call(sss.begin(), sss.end(), args); \
-        std::cout << "\n";                            \
-    }
-
-struct debugger
-{
-    typedef std::string::iterator si;
-    static void call(si it, si ed) {}
-    template <typename T, typename... aT>
-    static void call(si it, si ed, T a, aT... rest)
-    {
-        std::string b;
-        for (; *it != ','; ++it)
-            if (*it != ' ')
-                b += *it;
-        std::cout << b << " = " << a << " ";
-        call(++it, ed, rest...);
-    }
-};
-
-// Debug Memory
-// REF: https://stackoverflow.com/questions/38557168/does-vs-code-have-a-memory-viewer-and-or-a-disassembler-for-c-extension
-namespace mem
-{
-    std::string IntToHexa(int num)
-    {
-        int values[2];
-        int rest;
-        for (int i = 0; i < 2; i++)
-        {
-            rest = num % 16;
-            if (rest == 0 && num > 0)
-            {
-                values[i] = num / 16;
-            }
-            else
-            {
-                values[i] = num % 16;
-            }
-            num -= values[i] * std::pow(16, i);
-
-            if (values[i] > 9)
-            {
-                values[i] = 65 + values[i] - 10;
-            }
-        }
-
-        std::string output;
-        for (int i = 1; i > -1; i--)
-        {
-            if (values[i] > 10)
-            {
-                output += char(values[i]);
-            }
-            else
-            {
-                output += std::to_string(values[i]);
-            }
-        }
-        return output;
-    }
-
-    template <typename POINTER>
-    void MemView(POINTER pointer, int length = 10, int lines = 1)
-    {
-        unsigned char *ptr = (unsigned char *)pointer;
-        for (int x = 0; x < lines; x++)
-        {
-            std::cout << (void *)ptr << "   ";
-            for (int i = 0; i < length; i++)
-            {
-                std::cout << IntToHexa((int)*ptr) << "  ";
-                ptr++;
-            }
-            std::cout << std::endl;
-        }
-    }
-}
-/* #endregion */
 
 /* #region(collapsed) FUNCTIONS */
 // collected from stackoverflow
@@ -166,7 +51,6 @@ std::vector<std::string> split(const std::string str, const std::string regex_st
     std::vector<std::string> list(std::sregex_token_iterator(str.begin(), str.end(), regexz, -1), std::sregex_token_iterator());
     return list;
 }
-
 /* #endregion */
 
 using namespace std;

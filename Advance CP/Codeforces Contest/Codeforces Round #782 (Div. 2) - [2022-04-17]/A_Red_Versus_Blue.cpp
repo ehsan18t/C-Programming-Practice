76c1ab62b16a463e3e92a50be19ce35856102247
Ahsan Khan <nobody.ahsan@gmail.com>
2022-04-17 23:32:08 +0600

    Codeforces Round #782 (Div. 2)

---
+++ Advance CP/Codeforces Contest/Codeforces Round #782 (Div. 2) - [2022-04-17]/A_Red_Versus_Blue.cpp
@@ -0,0 +1,111 @@
+#include <bits/stdc++.h>
+
+// Iterator
+#define FOR(i, n) for (int i = 0; i < n; i++)
+#define FORr(i, n) for (int i = n - 1; i >= 0; i--)
+#define FORx(i, x, n) for (int i = x; i < n; i++)
+#define FORxr(i, x, n) for (int i = n - 1; i >= x; i--)
+
+// Data Manupulation
+#define init(A, V) memset(A, V, sizeof(A))
+
+// Math
+#define PI 2 * acos(0.0)
+#define INF LLONG_MAX
+#define sqr(n) (n * n)
+#define nCr(n, k) std::exp(std::lgamma(n + 1) - std::lgamma(k + 1) - std::lgamma(n - k + 1))
+
+// In/out
+#ifndef ONLINE_JUDGE
+#define in freopen("input.txt", "r", stdin)
+#define out freopen("output.txt", "w", stdout)
+#else
+#define in printf("")
+#define out printf("")
+#endif // !ONLINE_JUDGE
+
+// Data types
+#define db double
+#define ll long long
+#define ull unsigned long long
+
+// Lazy
+#define pb push_back
+#define lg length()
+#define sz size()
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+
+// Opt
+#define sync                      \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+/* #region(collapsed) FUNCTIONS */
+// collected from stackoverflow
+// REF: https://stackoverflow.com/questions/14265581/parse-split-a-string-in-c-using-string-delimiter-standard-c
+std::vector<std::string> split(const std::string str, const std::string regex_str)
+{
+    std::regex regexz(regex_str);
+    std::vector<std::string> list(std::sregex_token_iterator(str.begin(), str.end(), regexz, -1), std::sregex_token_iterator());
+    return list;
+}
+/* #endregion */
+
+using namespace std;
+
+int diff(int a, int b)
+{
+    return (a > b ? a - b : b - a);
+}
+
+// Solution
+// does not works
+void solve()
+{
+    bool loop = true, tie = false;
+    int n, r, b;
+    int rc = 0, bc = 0;
+    cin >> n >> r >> b;
+    string str = "";
+    int count = ceil(n / (b + 1));
+    int i = 0;
+    while (i < n && loop)
+    {
+        if (diff(r - rc, b - bc) <= 1)
+            tie = true;
+        int j = 0;
+        while (j < count && loop)
+        {
+            str += "R";
+            j++;
+            rc++;
+
+            if (i + j >= n)
+                loop = false;
+
+            if (j == 1 && tie)
+                break;
+        }
+        if (i + j < n)
+        {
+            str += "B";
+            bc++;
+        }
+        i += j + 1;
+    }
+    cout << str << endl;
+}
+
+int main()
+{
+    sync;
+    in;
+    out;
+    int n;
+    cin >> n;
+    while (n--)
+        solve();
+    return 0;
+}

9477e8a38f8bb798550078f8105d65000fb7f3d6
Ahsan Khan <mkhan201122@bscse.uiu.ac.bd>
2022-08-27 11:32:28 +0600

    Create Template.cpp

---
+++ DSA-Lab-2/Eval-2-Prep/Template.cpp
@@ -0,0 +1,87 @@
+#include <bits/stdc++.h>
+
+// Iterator
+#define FOR(i, n) for (int i = 0; i < n; i++)           // normal loop (0 to n)
+#define FORr(i, n) for (int i = n - 1; i >= 0; i--)     // normal reverse loop (n to 0)
+#define FORx(i, x, n) for (int i = x; i < n; i++)       // loop starts from x (x to n)
+#define FORxr(i, x, n) for (int i = n - 1; i >= x; i--) // reverse loop starts from n to x (n to x)
+#define for_map(m) for (auto it : m)                    // iterate map
+
+// Lazy Map
+#define contains(m, k) (m.find(k) != m.end()) // checks for k in map and returns boolean value
+#define K it.first
+#define V it.second
+
+// Data Manipulation
+#define init(A, V) memset(A, V, sizeof(A))
+
+// Math
+#define PI 2 * acos(0.0)
+#define INF LLONG_MAX
+#define sqr(n) (n * n)
+#define nCr(n, k) std::exp(std::lgamma(n + 1) - std::lgamma(k + 1) - std::lgamma(n - k + 1))
+
+// In/out
+#ifndef ONLINE_JUDGE
+#define in freopen("in.txt", "r", stdin)
+#define out freopen("out.txt", "w", stdout)
+#else
+#define in printf("")
+#define out printf("")
+#endif // !ONLINE_JUDGE
+
+// Data types
+#define ll long long
+#define ull unsigned long long
+#define umap unordered_map
+
+// Lazy
+#define psb push_back
+#define ppb pop_back
+#define lg length()
+#define sz size()
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+
+using namespace std;
+
+/////////////////////
+//                 //
+//     Solution    //
+//                 //
+/////////////////////
+
+/////////////////////////////////////////////////////
+/////////////////////////////////////////////////////
+// Input Array
+#define input(arr, n)           \
+    for (int i = 0; i < n; i++) \
+        cin >> arr[i];
+
+#define input2d(arr, n, m)          \
+    for (int i = 0; i < n; i++)     \
+        for (int j = 0; j < m; j++) \
+            cin >> arr[i][j];
+
+#define cumulative_sum(arr, out, n) \
+    out[0] = arr[0];                \
+    for (int i = 1; i < n; i++)     \
+        out[i] = out[i - 1] + arr[i];
+
+#define get_sum_i_to_j(out, i, j) (out[j] - out[i - 1])
+/////////////////////////////////////////////////////
+/////////////////////////////////////////////////////
+
+void solve(int cnt)
+{
+    // Codes
+}
+
+int main()
+{
+    int n = 1, i = 1;
+    cin >> n;
+    while (n--)
+        solve(i++);
+    return 0;
+}

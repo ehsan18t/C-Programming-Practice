69b7cf15cfe08c98adf402beca639f58171d1c1e
Ahsan Khan <mkhan201122@bscse.uiu.ac.bd>
2022-08-27 09:08:25 +0600

    Update clean.py

--- clean.py
+++ clean.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python
 import os
 
-def run_fast_scandir(dir, ignore = [], inc_ext = []):    # dir: str, ext: list
+# Configs
+remove_ext_less = True  # remove files without ext
+include_ext = ["exe", "bin"]    # Remove File Types
+ignore_folder_list = [".git", ".vscode"]    # Ignore Folders from Scanning
+dir_name = os.getcwd()  # Current Directory
+
+# Functions
+def run_fast_scandir(dir, ignore = [], inc_ext = [], remove_ext_less = False):    # dir: str, ext: list
     subfolders, files = [], []
 
     for f in os.scandir(dir):
@@ -12,11 +19,11 @@ def run_fast_scandir(dir, ignore = [], inc_ext = []):    # dir: str, ext: list
             #     print(f.name)   # ignored folders
         if f.is_file(): # filter
             ext = f.name.split('.')[-1]
-            if (ext == f.name) or (ext in inc_ext):
+            if (ext == f.name and remove_ext_less) or (ext in inc_ext):
                 files.append(f.path)
 
     for dir in list(subfolders):
-        sf, f = run_fast_scandir(dir, ignore, inc_ext)
+        sf, f = run_fast_scandir(dir, ignore, inc_ext, remove_ext_less)
         subfolders.extend(sf)
         files.extend(f)
     return subfolders, files
@@ -26,10 +33,9 @@ def deleteFile(filename):
     if os.path.exists(filename) and not os.path.isdir(filename) and not os.path.islink(filename):
         os.remove(filename)
 
-include_ext = ["exe", "bin"]
-ignore_folder_list = [".git", ".vscode"]
-dir_name = os.getcwd()
-sf, file_list = run_fast_scandir(dir_name, ignore_folder_list, include_ext)
+
+# Main
+sf, file_list = run_fast_scandir(dir_name, ignore_folder_list, include_ext, remove_ext_less)
 
 for item in file_list:
     print(f"-> Removing \"{item}\"")

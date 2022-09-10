8f416e4cb743e326e19b814651452f825c938f9f
Ahsan Khan <30434801+Ahsan40@users.noreply.github.com>
2022-09-10 21:59:25 +0600

    update dsa2 classes

--- DSA-Lab-2/Eval-2-Prep/Template.cpp
+++ DSA-Lab-2/Eval-2-Prep/Template.cpp
@@ -1,47 +1,9 @@
 #include <bits/stdc++.h>
-
-// Iterator
-#define FOR(i, n) for (int i = 0; i < n; i++)           // normal loop (0 to n)
-#define FORr(i, n) for (int i = n - 1; i >= 0; i--)     // normal reverse loop (n to 0)
-#define FORx(i, x, n) for (int i = x; i < n; i++)       // loop starts from x (x to n)
-#define FORxr(i, x, n) for (int i = n - 1; i >= x; i--) // reverse loop starts from n to x (n to x)
-#define for_map(m) for (auto it : m)                    // iterate map
-
-// Lazy Map
-#define contains(m, k) (m.find(k) != m.end()) // checks for k in map and returns boolean value
-#define K it.first
-#define V it.second
-
-// Data Manipulation
-#define init(A, V) memset(A, V, sizeof(A))
-
-// Math
-#define PI 2 * acos(0.0)
-#define INF LLONG_MAX
-#define sqr(n) (n * n)
-#define nCr(n, k) std::exp(std::lgamma(n + 1) - std::lgamma(k + 1) - std::lgamma(n - k + 1))
+#define INF 99999999
 
 // In/out
-#ifndef ONLINE_JUDGE
 #define in freopen("in.txt", "r", stdin)
 #define out freopen("out.txt", "w", stdout)
-#else
-#define in printf("")
-#define out printf("")
-#endif // !ONLINE_JUDGE
-
-// Data types
-#define ll long long
-#define ull unsigned long long
-#define umap unordered_map
-
-// Lazy
-#define psb push_back
-#define ppb pop_back
-#define lg length()
-#define sz size()
-#define yes cout << "YES" << endl
-#define no cout << "NO" << endl
 
 using namespace std;
 
@@ -72,15 +34,144 @@ using namespace std;
 /////////////////////////////////////////////////////
 /////////////////////////////////////////////////////
 
+typedef pair<int, int> edge;
+
+class graph
+{
+    int totalWeight;
+    int V;
+    list<edge> *adj;
+    int weights[100][100];
+
+public:
+    graph(int V)
+    {
+        this->V = V;
+        adj = new list<edge>[V];
+        totalWeight = 0;
+    }
+
+    void addEdge(int u, int v, int w)
+    {
+        adj[u].push_back(make_pair(v, w));
+        adj[v].push_back(make_pair(u, w));
+    }
+
+    void printAdjacency(int x)
+    {
+        list<pair<int, int>>::iterator i;
+        for (i = adj[x].begin(); i != adj[x].end(); ++i)
+        {
+            cout << (*i).first << "(" << (*i).second << ")"
+                 << " ";
+        }
+
+        // for(auto &i: adj[x])
+        // cout<<this->V;
+    }
+
+    void primMST()
+    {
+        priority_queue<edge, vector<edge>, greater<edge>> pq;
+        int src = 0;
+        vector<int> key(V, INF);
+        vector<int> parent(V, -1);
+        vector<bool> inMST(V, false);
+        pq.push(make_pair(0, src));
+        key[src] = 0;
+        while (!pq.empty()) /// While The PQ is not Empty
+        {
+            int u = pq.top().second; /// catch the top element of PQ
+            pq.pop();                /// Remove the top element of PQ
+            if (inMST[u] == true)    /// Sanity check ( if the element is already visited)
+                continue;
+            inMST[u] = true;
+            list<pair<int, int>>::iterator i;
+            for (i = adj[u].begin(); i != adj[u].end(); ++i) /// Exploring all the adjacency vertex of the element
+            {
+                int v = (*i).first;
+                int weight = (*i).second;
+                if (inMST[v] == false && weight < key[v]) /// (if it is already visited and if the current explored weight is less than the previously recorded weight)
+                {
+                    // cout << "Test: " << v << endl;
+                    key[v] = weight;               /// key Update with current explored weight
+                    pq.push(make_pair(key[v], v)); /// push the (weight, vertex) PQ
+                    parent[v] = u;                 /// parent update
+                    totalWeight += weight;         /// total weight update
+                }
+            }
+        }
+    }
+
+    void assignZero(int u, int v)
+    {
+        weights[u][v] = adj[u].front().second;
+        weights[v][u] = adj[v].front().second;
+        adj[u].front().second = 0;
+        adj[v].front().second = 0;
+    }
+
+    void assignOriginal(int u, int v)
+    {
+        adj[u].front().second = weights[u][v];
+    }
+
+    int mstWeight()
+    {
+        return totalWeight;
+    }
+};
+
 void solve(int cnt)
 {
-    // Codes
+    int v, e, q;
+    cin >> v >> e >> q;
+    graph g(v);
+
+    // input edges
+    for (int i = 0; i < e; i++)
+    {
+        int x, y, w;
+        cin >> x >> y >> w;
+        g.addEdge(x, y, w);
+    }
+
+    // generate tree
+    g.primMST();
+    // g.printAdjacency(1);
+
+    // query
+    while (q--)
+    {
+        int opt, a, b;
+        cin >> opt;
+        switch (opt)
+        {
+        case 1:
+            a, b;
+            cin >> a >> b;
+            g.assignZero(a, b);
+            break;
+        case 2:
+            a, b;
+            cin >> a >> b;
+            g.assignOriginal(a, b);
+            break;
+        case 3:
+            cout << g.mstWeight() << endl;
+            break;
+        default:
+            break;
+        }
+    }
 }
 
 int main()
 {
+    in;
+    out;
     int n = 1, i = 1;
-    cin >> n;
+    // cin >> n;
     while (n--)
         solve(i++);
     return 0;

5af287ce2b612ca30e23c9c2be875ce96d6f6bb6
Ahsan Khan <30434801+ehsan18t@users.noreply.github.com>
2022-12-18 23:23:50 +0600

    Codeforces Round #839 (Div. 3)

---
+++ Advance CP/Codeforces Contest/09. [18-12-2022] - Codeforces Round #839 (Div. 3)/B_Matrix_Rotation.cpp
@@ -0,0 +1,107 @@
+#include <bits/stdc++.h>
+
+// Iterator
+#define FOR(i, n) for (int i = 0; i < n; i++)
+#define FORr(i, n) for (int i = n - 1; i >= 0; i--)
+#define FORx(i, x, n) for (int i = x; i < n; i++)
+#define FORxr(i, x, n) for (int i = n - 1; i >= x; i--)
+#define for_map(m) for (auto it : m)
+
+// Lazy Map
+#define contains(m, k) (m.find(k) != m.end())
+#define K it.first
+#define V it.second
+
+// Data Manipulation
+#define init(A, V) memset(A, V, sizeof(A))
+
+// Math
+#define PI 2 * acos(0.0)
+#define INF(type) std::numeric_limits<type>::max()
+#define sqr(n) (n * n)
+#define nCr(n, k) std::exp(std::lgamma(n + 1) - std::lgamma(k + 1) - std::lgamma(n - k + 1))
+
+// In/out
+#ifndef ONLINE_JUDGE
+#define in freopen("in.txt", "r", stdin)
+#define out freopen("out.txt", "w", stdout)
+#else
+#define in printf("")
+#define out printf("")
+#endif // !ONLINE_JUDGE
+
+// Data types
+#define dbl double
+#define ll long long
+#define ull unsigned long long
+#define umap unordered_map
+
+// Lazy
+#define psb push_back
+#define ppb pop_back
+#define lg length()
+#define sz size()
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+
+// Opt
+#define sync                      \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+// REF: https://stackoverflow.com/questions/14265581/parse-split-a-string-in-c-using-string-delimiter-standard-c
+std::vector<std::string> split(const std::string str, const std::string regex_str)
+{
+    std::regex regexz(regex_str);
+    std::vector<std::string> list(std::sregex_token_iterator(str.begin(), str.end(), regexz, -1), std::sregex_token_iterator());
+    return list;
+}
+
+using namespace std;
+
+// Solution
+vector<int> rotate(vector<int> m)
+{
+    vector<int> res(4);
+    res[0] = m[2];
+    res[1] = m[0];
+    res[2] = m[3];
+    res[3] = m[1];
+    return res;
+}
+
+bool check(vector<int> m)
+{
+    if (m[0] < m[1] && m[2] < m[3] && m[0] < m[2] && m[1] < m[3])
+        return true;
+    return false;
+}
+
+void solve()
+{
+    vector<int> m(4);
+    cin >> m[0] >> m[1] >> m[2] >> m[3];
+    FOR(i, 4)
+    {
+        if (check(m))
+        {
+            yes;
+            return;
+        }
+        m = rotate(m);
+    }
+    no;
+}
+
+int main()
+{
+    sync;
+    in;
+    out;
+    int n = 1;
+    cin >> n;
+    while (n--)
+        solve();
+    return 0;
+}

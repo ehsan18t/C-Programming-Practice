efb616fac1dbe7e2eaf6e4ea022238ddbe985b7b
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2023-07-13 13:16:12 +0600

    ph-1-c0

---
+++ YouKnowWho-Academy/Cheat Sheet #1/A_Say_Hello_With_C.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+// Opt
+#define sync                      \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+using namespace std;
+
+// Solution
+void solve()
+{
+    string s;
+    cin >> s;
+    cout << "Hello, " << s << endl;
+}
+
+int main()
+{
+    sync;
+    int n = 1;
+    // cin >> n;
+    while (n--)
+        solve();
+    return 0;
+}
---
+++ YouKnowWho-Academy/Cheat Sheet #1/B_Basic_Data_Types.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+// Opt
+#define sync                      \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+using namespace std;
+
+#define in_out(var) \
+    cin >> var;     \
+    cout << var << endl;
+
+// Solution
+void solve()
+{
+    int a;
+    in_out(a);
+    long long b;
+    in_out(b);
+    char c;
+    in_out(c);
+    float d;
+    in_out(d);
+    double e;
+    in_out(e);
+}
+
+int main()
+{
+    sync;
+    int n = 1;
+    // cin >> n;
+    while (n--)
+        solve();
+    return 0;
+}

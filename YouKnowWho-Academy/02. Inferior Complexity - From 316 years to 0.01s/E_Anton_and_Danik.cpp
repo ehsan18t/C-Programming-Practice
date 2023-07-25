13bd7d4bd8ce540dc2e1f1deb4e739450830e1ea
Ehsan Khan <30434801+ehsan18t@users.noreply.github.com>
2023-07-26 00:49:21 +0600

    ph-1-c2-p1

---
+++ YouKnowWho-Academy/02. Inferior Complexity - From 316 years to 0.01s/E_Anton_and_Danik.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+
+#define INF(type) std::numeric_limits<type>::max()
+
+// Iterator
+#define FOR(i, n) for (int i = 0; i < n; i++)
+#define FORr(i, n) for (int i = n - 1; i >= 0; i--)
+#define FORx(i, x, n) for (int i = x; i < n; i++)
+#define FORxr(i, x, n) for (int i = n - 1; i >= x; i--)
+#define for_map(m) for (auto it : m)
+
+// Lazy Map
+#define contains(m, k) (m.find(k) != m.end())
+#define K it.first
+#define V it.second
+
+// Data Manipulation
+#define init(A, V) memset(A, V, sizeof(A))
+
+// Math
+#define PI 2 * acos(0.0)
+#define INF(type) std::numeric_limits<type>::max()
+#define sqr(n) (n * n)
+#define nCr(n, k) std::exp(std::lgamma(n + 1) - std::lgamma(k + 1) - std::lgamma(n - k + 1))
+
+// In/out
+#ifndef ONLINE_JUDGE
+#define in freopen("in.txt", "r", stdin)
+#define out freopen("out.txt", "w", stdout)
+#else
+#define in printf("")
+#define out printf("")
+#endif // !ONLINE_JUDGE
+
+// Data types
+#define dbl double
+#define ll long long
+#define ull unsigned long long
+#define umap unordered_map
+
+// Lazy
+#define psb push_back
+#define ppb pop_back
+#define lg length()
+#define sz size()
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+
+// Opt
+#define fastIO                    \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define endl "\n"
+
+using namespace std;
+
+// Solution
+void solve()
+{
+    int n, ans = 0;
+    umap<char, int> m;
+    string s;
+    cin >> n >> s;
+    FOR(i, n)
+    {
+        m[s[i]]++;
+    }
+    cout << (m['A'] == m['D'] ? "Friendship" : (m['D'] > m['A'] ? "Danik" : "Anton")) << endl;
+}
+
+int main()
+{
+    fastIO;
+    // in;
+    // out;
+    int n = 1;
+    // cin >> n;
+    while (n--)
+        solve();
+    return 0;
+}
